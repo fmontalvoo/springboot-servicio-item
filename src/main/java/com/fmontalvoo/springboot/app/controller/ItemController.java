78ab46587777bb65547117b338e675a4c9af0166
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-19 14:30:54 -0500

    Se crea controlador para servicio de items para los detalles de la factura.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

---
+++ src/main/java/com/fmontalvoo/springboot/app/controller/ItemController.java
@@ -0,0 +1,31 @@
+package com.fmontalvoo.springboot.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fmontalvoo.springboot.app.models.Item;
+import com.fmontalvoo.springboot.app.service.ItemService;
+
+@RestController
+@RequestMapping("/items")
+public class ItemController {
+
+	@Autowired
+	private ItemService service;
+
+	@GetMapping
+	public List<Item> findAll() {
+		return service.findAll();
+	}
+
+	@GetMapping("/{id}/{cantidad}")
+	public Item findById(@PathVariable Long id, @PathVariable Integer cantidad) {
+		return service.findById(id, cantidad);
+	}
+
+}
