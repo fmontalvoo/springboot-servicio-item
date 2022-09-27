88f50aa7dd30bd05b911a1356024692ca42cb331
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-09-26 21:11:07 -0500

    Se cambian los parametros para CircuitBreaker.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/main/java/com/fmontalvoo/springboot/app/AppConfig.java
+++ src/main/java/com/fmontalvoo/springboot/app/AppConfig.java
@@ -1,8 +1,16 @@
 package com.fmontalvoo.springboot.app;
 
+import java.time.Duration;
+
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +18,15 @@ public class AppConfig {
 	@Bean("clienteRest")
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> {
+			return new Resilience4JConfigBuilder(id).circuitBreakerConfig(CircuitBreakerConfig.custom()
+					.slidingWindowSize(10).failureRateThreshold(50).waitDurationInOpenState(Duration.ofSeconds(10L))
+					.permittedNumberOfCallsInHalfOpenState(5).build()).timeLimiterConfig(TimeLimiterConfig.ofDefaults())
+					.build();
+		});
 	}
 }
